45cf0b0a4020190caee6fa0391d6df965d518328
runnphoenix <runnphoenix@gmail.com>
2021-03-18 22:33:38 +0800

    1. add output details in minizinc, now we can use minizinc in terminal with -o option to write to txt files; 2. imlemented a test script in python which could display the result in terminal with colors

---
+++ result-check.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import sys
+
+def get_test_file_location():
+	args = sys.argv
+	if len(args) < 2:
+			print("Please input the location of the json file.")
+	elif len(args) > 2:
+			print("You can only input one json file location as parameter.")
+	else:
+			test_file_path = sys.argv[1]
+			return test_file_path
+	
+
+test_file_path = get_test_file_location()
+
+with open(test_file_path, 'r') as FI:
+	line1 = FI.readline()
+	wh=line1.split()
+	w,h = int(wh[0]), int(wh[1])
+	line2 = FI.readline()
+	n = int(line2)
+	
+	sheet = np.zeros((h,w), dtype=np.int)
+	
+	for i in range(n):
+		line = FI.readline()
+		piece = line.split()
+		wi = int(piece[0])
+		hi = int(piece[1])
+		xi = int(piece[2])
+		yi = int(piece[3])
+		
+		sheet[h-yi-hi:h-yi, xi:xi+wi] = i+1
+	
+	def get_color_coded_background(i):
+		return "\033[4{}m {:2} \033[0m".format(i%7, i) # 3{}m-forground 4{}m-background, %7 as color of 38m is gray
+	
+	def print_a_ndarray(map, row_sep=" "):
+		n, m = map.shape
+		fmt_str = "\n".join([row_sep.join(["{}"]*m)]*n)
+		print(fmt_str.format(*map.ravel()))
+
+	sheet_modified = np.vectorize(get_color_coded_background)(sheet)
+	print_a_ndarray(sheet_modified, row_sep="")
+	
+	FI.close()
