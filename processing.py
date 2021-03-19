cc1d973573228514c19d7def5a0c3f2983e31717
runnphoenix <runnphoenix@gmail.com>
2021-03-19 16:29:32 +0800

    add a python script file named processing.py to process all the minizinc input files and record the processing time for each problem

---
+++ processing.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+from datetime import datetime
+import time
+
+process_time = {}
+
+list_of_files = glob.glob('./*.dzn') 
+
+for data_file in list_of_files:
+	print("Processing " + data_file + "...")
+	file_name = data_file.split('.')[1][1:]
+	result_file = file_name + '-out.txt'
+	
+	start_time = time.time()
+	process = os.popen('minizinc ' + '-o ' + result_file + ' -t ' + '60000 ' + 'PWP.mzn ' + data_file)
+	response = process.read()
+	end_time = time.time()
+	process_time[file_name] = end_time - start_time
+	
+	print(response)
+	process.close()
+	
+	print(process_time)
